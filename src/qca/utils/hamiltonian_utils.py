a87aa337c0c6d1f252c1bb9e22af795edf15e1e8
Jonhas Colina <jonhas@lanl.gov>
2024-02-22 09:26:55 -0700

    successfull running of magnetic lattice using pyliqtr1.0 ontop of modularized functionality

--- src/qca/utils/hamiltonian_utils.py
+++ src/qca/utils/hamiltonian_utils.py
@@ -1,6 +1,8 @@
-from openfermion import FermionOperator
-from networkx import relabel_nodes, Graph
-
+import random
+from networkx import relabel_nodes, Graph, grid_graph
+from networkx.generators.lattice import grid_2d_graph
+from openfermion import FermionOperator, QubitOperator
+from pyLIQTR.utils import Hamiltonian
 
 def flatten_nx_graph(graph: Graph) -> Graph:
     new_ids = {}
@@ -124,3 +126,91 @@ def nx_to_two_orbital_hamiltonian(
         H += FermionOperator(((i, 1), (i, 0)), -mu)
 
     return H
+
+# given a networkx graph g, construct a hamiltonian with random weights
+# which can be processed by pyLIQTR
+def nx_longitudinal_ising_terms(graph,p,magnitude=1) -> list[tuple[str, float]]:
+    H_longitudinal = []
+    n = len(graph.nodes)
+    for n1, n2 in graph.edges:
+        weight = magnitude if random.random() < p else -magnitude
+        curr_hamil_string = n * 'I' 
+        for idx in range(len(graph)):
+            if idx == n1 or idx == n2:
+                curr_hamil_string = f'{curr_hamil_string[:idx]}Z{curr_hamil_string[idx+1:]}'
+        H_longitudinal.append((curr_hamil_string, weight))
+    return H_longitudinal
+
+def nx_transverse_ising_terms(graph: Graph,p,magnitude=0.1) -> list[tuple[str, float]]:
+    H_transverse = []
+    n = len(graph)
+    for idx in range(n):
+        w = magnitude if random.random() < p else -magnitude
+        curr_hamil_string = n * 'I' 
+        for k in range(n):
+            if idx == k:
+                curr_hamil_string = f'{curr_hamil_string[:idx]}X{curr_hamil_string[idx+1:]}'
+        H_transverse.append((curr_hamil_string, w))
+    return H_transverse
+
+
+def nx_triangle_lattice(lattice_size: int) -> Graph:
+    graph = grid_2d_graph(lattice_size, lattice_size)
+    for i in range(lattice_size - 1):
+        for j in range(lattice_size - 1):
+            graph.add_edge((i,j),(i+1,j+1))
+    return graph
+
+def generate_square_hamiltonian(lattice_size: int, dim:int, longitudinal_weight_prob:float=0.5, transverse_weight_prob:float=1, is_trangle_hamiltoniain:bool=False):
+    if is_trangle_hamiltoniain:
+        graph = nx_triangle_lattice(lattice_size)
+    else:
+        dimensions = (lattice_size, lattice_size) if dim == 2 else (lattice_size, lattice_size, lattice_size)
+        graph = grid_graph(dim=dimensions)
+    graph = flatten_nx_graph(graph)
+    H_transverse = nx_transverse_ising_terms(graph, transverse_weight_prob)
+    H_longitudinal = nx_longitudinal_ising_terms(graph, longitudinal_weight_prob)
+    return H_transverse, H_longitudinal
+
+def pyliqtr_hamiltonian_to_openfermion_qubit_operator(H:Hamiltonian) -> QubitOperator:
+    open_fermion_operator = QubitOperator()
+    for term in H.terms:
+        open_fermion_term = ''
+        for i, pauli in enumerate(term[0]):
+            if pauli != 'I':
+                open_fermion_term = f'{open_fermion_term}{pauli}{i} '
+        open_fermion_term_op = QubitOperator(open_fermion_term)
+        if open_fermion_term:
+            open_fermion_operator += term[1] * open_fermion_term_op
+    return open_fermion_operator
+
+def assign_hexagon_labels(graph:Graph):
+    for n1, n2 in graph.edges:
+        # start by making sure that the edges are ordered correctly
+        r1,c1 = n1
+        r2,c2 = n2
+        if r2 - r1 < 0 or c2 - c1 < 0:
+            r1, r2 = r2, r1
+            c1, c2 = c2, c1
+        
+        # now that they are ordered correctly, we can assign labels
+        label = ''
+        if c1 == c2:
+            label = 'Z'
+        # You can differentiate X and Y labels based off nx's node label parity
+        elif (((r1 % 2) + (c1 % 2)) % 2 == 0):
+            label = 'Y'
+        else:
+            label = 'X'
+        
+        graph[n1][n2]['label'] = label
+
+def assign_directional_triangular_labels(g:Graph, lattice_size:int) -> None: 
+    for i in range(lattice_size - 1):
+        for j in range(lattice_size - 1):
+            g[(i,j)][(i+1,j)]['label'] = 'Z'
+            g[(i,j)][(i,j+1)]['label'] = 'X'
+            g[(i,j)][i+1,j+1]['label'] = 'Y'
+        g[(i,lattice_size-1)][(i+1,lattice_size-1)]['label'] = 'Z'
+    for j in range(lattice_size - 1):
+        g[(lattice_size-1,j)][(lattice_size-1,j+1)]['label'] = 'X'
