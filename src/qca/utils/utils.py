6f08d4e2320f5a2224a94211703da8631ee33215
Jonhas Colina <jonhas@lanl.gov>
2024-03-21 14:09:56 -0600

    adding pdfs for changed notebooks. Modified notebooks to use estimate_qsp and estimate_trotter through utility functions to then be reused

--- src/qca/utils/utils.py
+++ src/qca/utils/utils.py
@@ -2,12 +2,13 @@ import os
 import re
 import json
 import time
+import pandas as pd
+import matplotlib.pyplot as plt
+from statistics import median
+from pyLIQTR.utils.utils import count_T_gates
 from cirq import Circuit, QasmOutput, AbstractCircuit
 from pyLIQTR.utils.qsp_helpers import circuit_decompose_once
 from pyLIQTR.gate_decomp.cirq_transforms import clifford_plus_t_direct_transform
-from pyLIQTR.utils.utils import count_T_gates
-import matplotlib.pyplot as plt
-import pandas as pd
 
 def count_gates(cpt_circuit: AbstractCircuit) -> int:
     count = 0
@@ -39,7 +40,7 @@ def get_T_depth_wire(cpt_circuit: AbstractCircuit):
             max_depth = count_dict[register]
     return max_depth
 
-def plot_T_step_histogram(cpt_circuit:AbstractCircuit, kwargs, lowest_ind:int=0) -> plt.hist:
+def get_T_width(cpt_circuit: AbstractCircuit) -> list[int]:
     t_widths = [0] * len(cpt_circuit)
     for i, moment in enumerate(cpt_circuit):
         width = 0
@@ -48,9 +49,14 @@ def plot_T_step_histogram(cpt_circuit:AbstractCircuit, kwargs, lowest_ind:int=0)
             if opstr[0] == 'T':
                 width += 1
         t_widths[i] = width
+    return t_widths
+
+def plot_T_step_histogram(cpt_circuit:AbstractCircuit, kwargs, lowest_ind:int=0) -> plt.hist:
+    t_widths = get_T_width(cpt_circuit)
     bins = range(max(t_widths))
-    histogram = plt.hist(t_widths, bins[lowest_ind:-1], **kwargs)
-    return histogram
+    y_label = bins[lowest_ind:-1]
+    histogram = plt.hist(t_widths, y_label, **kwargs)
+    return histogram, median(t_widths)
 
 def plot_histogram(cpt_circuit: AbstractCircuit,
                    histogram_title:str,
@@ -58,8 +64,9 @@ def plot_histogram(cpt_circuit: AbstractCircuit,
                    widthdir:str,
                    lowest_ind:int=0,
                    **kwargs) -> None:
-    circuit_histogram = plot_T_step_histogram(cpt_circuit, kwargs=kwargs, lowest_ind=lowest_ind)
-    plt.title(histogram_title)
+    circuit_histogram, median_t_width = plot_T_step_histogram(cpt_circuit, kwargs=kwargs, lowest_ind=lowest_ind)
+    median_t_width_text = f'median T width: {median_t_width}'
+    plt.title(f'{histogram_title} - {median_t_width_text}')
     plt.xlabel('T Width')
     plt.ylabel('Count')
     plt.savefig(f'{figdir}_width_histogram_square.pdf')
@@ -78,6 +85,7 @@ def get_T_depth(cpt_circuit: AbstractCircuit):
     return t_depth
 
 def gen_resource_estimate(cpt_circuit: AbstractCircuit,
+                          num_magnus:int = 1,
                           trotter_steps:int = -1,
                           circ_occurences:int = -1) -> dict:
     '''
@@ -104,39 +112,32 @@ def gen_resource_estimate(cpt_circuit: AbstractCircuit,
     if trotter_steps > 0:
         resource_estimate['total_t_depth'] = t_depth * trotter_steps
         resource_estimate['max_t_count_single_wire'] = t_depth_single_wire * trotter_steps
-        resource_estimate['trotter_steps_required'] = trotter_steps
+        resource_estimate['subcircuit_occurences'] = trotter_steps
     if circ_occurences > 0:
-        resource_estimate['circuit occurrences'] = circ_occurences
+        resource_estimate['subcicruit_occurrences'] = circ_occurences*num_magnus
 
     return resource_estimate
 
-def re_as_json(main_estimate:dict, estimates:list[dict], file_name:str) -> None:
-    main_estimate['subcircuit_info'] = {}
-    if estimates:
-        for op in estimates:
-            for op_key in op.keys():
-                main_estimate['subcircuit_info'][op_key] = op[op_key]
-    with open(file_name, 'w') as f:
-            json.dump(main_estimate, f,
-                    indent=4,
-                    separators=(',', ': '))
-def estimate_gsee(
-        circuit: Circuit,
-        outdir: str,
-        circuit_name: str = 'gse_circuit',
-        write_circuits: bool = False) -> None:
+
+def circuit_estimate(circuit:AbstractCircuit,
+                     outdir: str,
+                     circuit_name: str,
+                     num_magnus:int=1,
+                     timesteps:int=1,
+                     timestep_of_interest:int=1,
+                     trotter_steps:int=-1,
+                     write_circuits:bool = False) -> AbstractCircuit:
     if not os.path.exists(outdir):
         os.makedirs(outdir)
     
     subcircuit_counts = dict()
-
     for moment in circuit:
         for operation in moment:
             gate_type = type(operation.gate)
+            gate_type_name = f'{str(gate_type).split("/")[-1][:-2]}'
             if gate_type in subcircuit_counts:
                 subcircuit_counts[gate_type][0] += 1
             else:
-                gate_type_name = f'{str(gate_type).split(".")[-1][:-2]}'
                 t0 = time.perf_counter()
                 decomposed_circuit = circuit_decompose_once(circuit_decompose_once(Circuit(operation)))
                 t1 = time.perf_counter()
@@ -148,7 +149,6 @@ def estimate_gsee(
                 t1 = time.perf_counter()
                 elapsed = t1-t0
                 print(f'   Time to transform decomposed {gate_type_name} circuit to Clifford+T: {elapsed} seconds')
-                
                 if write_circuits:
                     outfile_qasm_decomposed = f'{outdir}{gate_type_name}.decomposed.qasm'
                     outfile_qasm_cpt = f'{outdir}{gate_type_name}.cpt.qasm'
@@ -160,7 +160,6 @@ def estimate_gsee(
                                cpt_circuit.all_qubits()).save(outfile_qasm_cpt)
     
                 subcircuit_counts[gate_type] = [1, cpt_circuit, gate_type_name]
-                
     total_gate_count = 0
     total_gate_depth = 0
     total_T_count = 0
@@ -172,6 +171,7 @@ def estimate_gsee(
         subcircuit = subcircuit_counts[gate][1]
         subcircuit_name = subcircuit_counts[gate][2]
         resource_estimate = gen_resource_estimate(subcircuit,
+                                                  num_magnus=num_magnus,
                                                   circ_occurences=subcircuit_counts[gate][0])
         subcircuit_info = {subcircuit_name:resource_estimate}
         subcircuit_re.append(subcircuit_info)
@@ -182,21 +182,34 @@ def estimate_gsee(
         t_count = resource_estimate['t_count']
         clifford_count = resource_estimate['clifford_count']
         
-        total_gate_count += subcircuit_counts[gate][0] * gate_count
-        total_gate_depth += subcircuit_counts[gate][0] * gate_depth
-        total_T_depth += subcircuit_counts[gate][0] * t_depth
-        total_T_depth_wire += subcircuit_counts[gate][0] * t_depth_wire
-        total_T_count += subcircuit_counts[gate][0] * t_count
-        total_clifford_count += subcircuit_counts[gate][0] * clifford_count
+        total_gate_count += subcircuit_counts[gate][0] * gate_count * timesteps / timestep_of_interest
+        total_gate_depth += subcircuit_counts[gate][0] * gate_depth * timesteps / timestep_of_interest
+        total_T_depth += subcircuit_counts[gate][0] * t_depth * timesteps / timestep_of_interest
+        total_T_depth_wire += subcircuit_counts[gate][0] * t_depth_wire * timesteps / timestep_of_interest
+        total_T_count += subcircuit_counts[gate][0] * t_count * timesteps / timestep_of_interest
+        total_clifford_count += subcircuit_counts[gate][0] * clifford_count * timesteps / timestep_of_interest
 
     outfile_data = f'{outdir}{circuit_name}_high_level.json'
+    circ_occurences = trotter_steps if trotter_steps > 0 else num_magnus
     total_resources = {
-        'num_qubits': len(subcircuit.all_qubits()),
+        'num_qubits': len(circuit.all_qubits()),
         'gate_count': total_gate_count,
         'circuit_depth': total_gate_depth,
         't_count': total_T_count,
         't_depth': total_T_depth,
-        't_depth_wire': total_T_depth_wire,
-        'clifford_count': total_clifford_count
+        'max_t_depth_wire': total_T_depth_wire,
+        'clifford_count': total_clifford_count,
+        'circuit_occurences': circ_occurences
     }
     re_as_json(total_resources, subcircuit_re, outfile_data)
+
+def re_as_json(main_estimate:dict, estimates:list[dict], file_name:str) -> None:
+    main_estimate['subcircuit_info'] = {}
+    if estimates:
+        for op in estimates:
+            for op_key in op.keys():
+                main_estimate['subcircuit_info'][op_key] = op[op_key]
+    with open(file_name, 'w') as f:
+            json.dump(main_estimate, f,
+                    indent=4,
+                    separators=(',', ': '))
