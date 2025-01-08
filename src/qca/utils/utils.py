bce39fe2bcc82b17f923a7aa401066a747e2aebf
gsgrattan <gsgrattan@gmail.com>
2025-01-08 12:12:40 -0700

    removed errors

--- src/qca/utils/utils.py
+++ src/qca/utils/utils.py
@@ -407,11 +407,6 @@ def grab_circuit_resources(circuit: cirq.AbstractCircuit,
     if metadata != None:
         if (metadata.value_per_circuit != None) and (metadata.repetitions_per_application != None):
             estimates['value_per_t_gate'] = metadata.value_per_circuit/(estimates.get('Logical_Abstract').get('t_count') * metadata.repetitions_per_application)
-        else:
-            raise ValueError("Circuit value or repetetions per application not defined")
-        
-    else: 
-        raise ValueError("Metadata object not defined")
     
     outfile = f'{outdir}{fname}_re.json'
     gen_json(estimates, outfile, metadata)
